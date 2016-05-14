9d57cc7ccf2a8c9f4d777b053fa6a0c4bf5aaa5a
Roderic Day <roderic.day@gmail.com>
2016-05-14 19:42:43 -0400

    Static serve

--- bin/deploy.sh
+++
@@ -1,8 +0,0 @@
-set -e
-../manage.py test ..
-git add -u
-git commit --amend --no-edit > /dev/null
-git push -f
-ssh roderic@projects.roderic.ca "cd forum && git reset --hard HEAD"
-ssh roderic@projects.roderic.ca ".venvs/notes/bin/python forum/manage.py migrate"
-ssh roderic@projects.roderic.ca "supervisorctl restart forum"
--- forum/urls.py
+++ forum/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import urls as auth_urls
 from django.contrib.auth.decorators import login_required
@@ -14,4 +16,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include(auth_urls)),
     url(r'^accounts/profile/$', home),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
