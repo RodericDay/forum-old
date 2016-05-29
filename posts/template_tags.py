79bb7c0ed330ad030045b76c847782b1d6906faf
Roderic Day <roderic.day@gmail.com>
2016-05-29 18:17:04 -0400

    Footer Tag

--- posts/template_tags.py
+++ posts/template_tags.py
@@ -1,4 +1,4 @@
-import re
+import re, itertools
 from django import template
 
 register = template.Library()
@@ -15,4 +15,13 @@ def bleach(text):
     allowed = r'b|i|em|u|s|pre|code|spoiler|blockquote|article|header|footer|center|right|strong'
     pattern = r'&lt;(/)?({})&gt;'.format(allowed)
     bleached = re.sub(pattern, r'<\1\2>', text)
+    # footers
+    for i in itertools.count(1):
+        pattern = '[[{i}]]'.format(i=i)
+        if bleached.count(pattern) != 2:
+            break
+        intext = '<sup><a id="{i}a" href="#{i}b">{i}</a></sup>'.format(i=i)
+        infoot = '<sup><a id="{i}b" href="#{i}a">{i}</a></sup>'.format(i=i)
+        bleached = bleached.replace(pattern, intext, 1)
+        bleached = bleached.replace(pattern, infoot, 1)
     return bleached
