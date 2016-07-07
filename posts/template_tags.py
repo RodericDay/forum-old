874479db47e0a64b2f490facba4b87ca13027063
Roderic Day <roderic.day@gmail.com>
2016-07-06 20:49:07 -0400

    Hide figure via tag

--- posts/template_tags.py
+++ posts/template_tags.py
@@ -13,20 +13,23 @@ def bleach(text):
     #hyperlink
     text = re.sub(r'&lt;a href=&quot;(\S+?)&quot;&gt;(.+?)&lt;/a&gt;', r'<a href="\1">\2</a>', text)
     text = re.sub(r'(\s|>|^)(\w+://\S+?)(\s|<|$)', r'\1<a href="\2">\2</a>\3', text)
+
     #misc
-    allowed = r'b|i|em|u|s|pre|code|spoiler|blockquote|article|header|footer|center|right|strong'
+    allowed = r'b|i|em|u|s|pre|code|spoiler|blockquote|article|header|footer|center|right|strong|figure'
     pattern = r'&lt;(/)?({})&gt;'.format(allowed)
-    bleached = re.sub(pattern, r'<\1\2>', text)
+    text = re.sub(pattern, r'<\1\2>', text)
+
     # footers
     for i in itertools.count(1):
         pattern = '[[{i}]]'.format(i=i)
-        if bleached.count(pattern) != 2:
+        if text.count(pattern) != 2:
             break
         intext = '<sup><a id="{i}a" href="#{i}b">{i}</a></sup>'.format(i=i)
         infoot = '<sup><a id="{i}b" href="#{i}a">{i}</a></sup>'.format(i=i)
-        bleached = bleached.replace(pattern, intext, 1)
-        bleached = bleached.replace(pattern, infoot, 1)
-    return bleached
+        text = text.replace(pattern, intext, 1)
+        text = text.replace(pattern, infoot, 1)
+
+    return text
 
 @register.filter(is_safe=True)
 def b64encode(text):
